cb2baf0f91a305a764174ede4b4bd433702f5a4b
Mehetaj <121656128+Mehetaj@users.noreply.github.com>
2023-05-20 10:07:31 +0600

    create 20 data and send to client side

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const port = process.env.PORT || 5000;
 // MiddleWare
 app.use(cors());
 app.use(express.json());
-
+require("dotenv").config();
 
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://toyMarketplace:axUVTCNxShzeYpzO@cluster0.4bdkenh.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4bdkenh.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://toyMarketplace:axUVTCNxShzeYpzO@cluster0.4bdkenh.mongodb.net/?retryWrites=true&w=majority`;
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4bdkenh.mongodb.net/?retryWrites=true&w=majority`;
 
 
@@ -37,6 +38,7 @@ async function run() {
     const ingCollection = client.db("Images").collection("Image");
     const tabsCollection= client.db("tabsCollection").collection("tabs");
     const toysCollection = client.db("toysCollection").collection("toys");
+    const postedCollection = client.db("postedCollection").collection("posts")
     // console.log(toysCollection);
 
     app.get('/imgs', async(req, res) => {
@@ -45,33 +47,30 @@ async function run() {
       res.send(result)
     })
 
-
-    app.get('/sportstoys', async (req, res) => {
-      const q = req.body;
-      const toys = toysCollection.find();
+    app.get('/posts',async(req, res) => {
+      const toys =  postedCollection.find();
       const result = await toys.toArray();
       res.send(result)
     })
 
 
-    // app.get('/sportstoys', async (req, res) => {
-    //   const q = req.body;
-    //   const toys = toysCollection.find();
-    //   const result = await toys.toArray();
-    //   res.send(result)
-    // })
-
-    // app.post('/sportstoys', async (req, res) => {
-    //   const toys = req.body;
-    //   const result = await toysCollection.insertOne(data)
-    //   res.send(result)
-    // })
-
-    app.post('/sportstoys', async (req, res) => {
-      const toy = req.body;
-      const result = await toysCollection.insertOne(toy);
+    app.get('/sportstoys', async (req, res) => {
+      const toys = toysCollection.find();
+      const result = await toys.toArray();
       res.send(result)
     })
+
+    app.post('/posts',async(req ,res) => {
+      const toy = req.body;
+      const result = await postedCollection.insertOne(toy);
+      res.send(result)
+    })
+
+    // app.post('/sportstoys', async (req, res) => {
+    //   const toy = req.body;
+    //   const result = await toysCollection.insertOne(toy);
+    //   res.send(result)
+    // })
 
     app.get('/sportstoys/:id', async (req, res) => {
       const id = req.params.id;
